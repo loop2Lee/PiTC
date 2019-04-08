c603928f7a1b7613b00fc187a2a8db4866d317e8
loop2Lee <31805920+loop2Lee@users.noreply.github.com>
2019-04-08 15:29:17 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,1 +1,22 @@
-
+/*
+API for PiTC
+Language: Node.js
+(c) 2018 All rights reserved except for copyrighted resources belonging to others.
+This file may contain privileged information.
+The distribution of this file is not permitted without explicit written permission of the original author.
+dependencies:
+pm2 -g
+apt htop
+promise
+express
+apt ufw
+url
+optional: node-2fa
+*/
+"use strict";
+Number.prototype.pad = function(size) {
+	let s = String(this);
+	while (s.length < (size || 2)) {s = "0" + s;}
+	return s;
+}
+const fs = require("fs");
