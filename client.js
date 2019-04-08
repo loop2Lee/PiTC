8f645407bd59b48658907e90c8e88f469082c774
loop2Lee <31805920+loop2Lee@users.noreply.github.com>
2019-04-08 16:28:22 -0400

    Update client.js

--- client.js
+++ client.js
@@ -1,22 +1,19 @@
-/*
-API for PiTC
-Language: Node.js
-(c) 2018 All rights reserved except for copyrighted resources belonging to others.
-This file may contain privileged information.
-The distribution of this file is not permitted without explicit written permission of the original author.
-dependencies:
-pm2 -g
-apt htop
-promise
-express
-apt ufw
-url
-optional: node-2fa
-*/
-"use strict";
-Number.prototype.pad = function(size) {
-	let s = String(this);
-	while (s.length < (size || 2)) {s = "0" + s;}
-	return s;
-}
-const fs = require("fs");
+var net = require('net');
+var client = new net.Socket();
+client.connet({
+	port:80
+});
+
+client.on('connect', function(){
+	//how to import temperature data from RPi?
+	client.write(temp);
+});
+
+client.on('data', function(data)){
+	  if (ans.fan){
+		//need turn on the fan 
+	  }
+	  else{
+		  //need turn off the fan
+	  }
+});
