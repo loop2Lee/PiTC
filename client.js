4a22f9c865795fbfc551cf49b81cbee426887c6a
loop2Lee <31805920+loop2Lee@users.noreply.github.com>
2019-04-10 17:03:23 -0400

    Update client.js

--- client.js
+++ client.js
@@ -8,11 +8,10 @@ client.connet({
 //http://address:port/report?t=&id=
 
 //http://address:port/report?t=80&id=2
-id = 1;
+id = 1; //each RPi with a sensor has a unique ID
 setInterval(() => {
 	child_process.exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr)) => {
 		temperature = stdout
-		id = id + 1
 	}
 	let url = "http://address:port/report?t=" + temperature + "&id=" + id;
 	request(url, (error, response, body) => {
@@ -21,7 +20,7 @@ setInterval(() => {
 			child_process.exec("sudo python3 LIGHT_ON.py")
 		}
 		else{
-			child_pross.exec("sudo python3 LIGHT_OFF.py")
+			child_process.exec("sudo python3 LIGHT_OFF.py")
 		}
 	});
 }, 5000);
