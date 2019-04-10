472740a639a42d7dcdb6abf4c9aa02fca227e5c1
loop2Lee <31805920+loop2Lee@users.noreply.github.com>
2019-04-10 16:50:28 -0400

    Update client.js

--- client.js
+++ client.js
@@ -8,31 +8,26 @@ client.connet({
 //http://address:port/report?t=&id=
 
 //http://address:port/report?t=80&id=2
-
+id = 1;
 setInterval(() => {
+	child_process.exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr)) => {
+		temperature = stdout
+		id = id + 1
+	}
 	let url = "http://address:port/report?t=" + temperature + "&id=" + id;
 	request(url, (error, response, body) => {
 		let answer = JSON.parse(body);
-		if (answer.fan == true) {}
-		else{}
+		if (answer.fan == true) {
+			child_process.exec("sudo python3 LIGHT_ON.py")
+		}
+		else{
+			child_pross.exec("sudo python3 LIGHT_OFF.py")
+		}
 	});
 }, 5000);
-client.on('connect', function(){
-	//how to import temperature data from RPi?
-	client.write(temp);
-});
-
-client.on('data', function(data)){
-	  if (ans.fan){
-		//need turn on the fan
-	  }
-	  else{
-		  //need turn off the fan
-	  }
-});
 
 /*
 {
-	fan: true
+	body:  fan: true
 }
 */
