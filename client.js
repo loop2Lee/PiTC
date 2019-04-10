07a032a5598d239700236c551602cda960c155e4
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-04-10 16:19:34 -0400

    update client to add request format for server URL endpoint

--- client.js
+++ client.js
@@ -1,9 +1,21 @@
 var net = require('net');
 var client = new net.Socket();
+let request = require("request");
 client.connet({
 	port:80
 });
+//http://address:port/report?t=&id=
 
+//http://address:port/report?t=80&id=2
+
+setInterval(() => {
+	let url = "http://address:port/report?t=" + temperature + "&id=" + id;
+	request(url, (error, response, body) => {
+		let answer = JSON.parse(body);
+		if (answer.fan == true) {}
+		else{}
+	});
+}, 5000);
 client.on('connect', function(){
 	//how to import temperature data from RPi?
 	client.write(temp);
@@ -11,9 +23,15 @@ client.on('connect', function(){
 
 client.on('data', function(data)){
 	  if (ans.fan){
-		//need turn on the fan 
+		//need turn on the fan
 	  }
 	  else{
 		  //need turn off the fan
 	  }
 });
+
+/*
+{
+	fan: true
+}
+*/
