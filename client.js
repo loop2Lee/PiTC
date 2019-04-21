b577483afa45740f4159f0f339ee241e6f927455
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-04-21 17:47:52 -0400

    Update client.js

--- client.js
+++ client.js
@@ -7,24 +7,41 @@ let child_process = require("child_process");
 
 //http://address:port/report?t=80&id=2
 const id = 1; //each RPi with a sensor has a unique ID
+output("client running");
 setInterval(() => {
-	child_process.exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr) => {
+	getTemperature(temperature => {
 		temperature = stdout.substring(5, 7);
 		let url = "http://" + process.argv[2] + "/report?t=" + temperature + "&id=" + id;
 		request(url, (error, response, body) => {
 			let answer = JSON.parse(body);
 			if (answer.fan == true) {
-				console.log("Temp: " + temperature + "    Fan: ON");
-				child_process.exec("sudo python3 LIGHT_ON.py", { timeout: 5000 });
+				output("Temp: " + temperature + "    Fan: ON");
+				child_process.exec("python3 LIGHT_ON.py", { timeout: 5000 });
 			}
 			else {
-				console.log("Temp: " + temperature + "    Fan: OFF");
-				child_process.exec("sudo python3 LIGHT_OFF.py", { timeout: 5000 });
+				output("Temp: " + temperature + "    Fan: OFF");
+				child_process.exec("python3 LIGHT_OFF.py", { timeout: 5000 });
 			}
 		});
-	});
+	}, true);
 }, 5000);
-
+function getTemperature(callback, random = false) {
+	if (!random) {//get CPU temperature
+		child_process.exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr) => {
+			temperature = stdout.substring(5, 7);
+			callback(temperature);
+		});
+	}
+	else {//get random value for temperature
+		callback(Math.trunc(Math.random() * 100));
+	}
+}
+function output(t) {//general utility function
+	if (exists(t)) {
+		let n = new Date().toISOString().slice(0, 19).replace('T', ' ');;
+		console.log(n + "." + new Date().getMilliseconds().pad(3) + " : " + t);
+	}
+}
 /*
 {
 	body:  fan: true
