b1ea62ad581486cd011c654bb9691e1f01fc54eb
iaace-NA <23423640+iaace-NA@users.noreply.github.com>
2019-04-21 16:54:03 -0400

    client code cleanup

--- client.js
+++ client.js
@@ -2,26 +2,23 @@ var net = require('net');
 var client = new net.Socket();
 let request = require('request');
 
-client.connet({
-	port:80
-});
 //http://address:port/report?t=&id=
 
 //http://address:port/report?t=80&id=2
-id = 1; //each RPi with a sensor has a unique ID
+const id = 1; //each RPi with a sensor has a unique ID
 setInterval(() => {
-	child_process.exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr)) => {
-		temperature = stdout
-	}
-	let url = "http://address:port/report?t=" + temperature + "&id=" + id;
-	request(url, (error, response, body) => {
-		let answer = JSON.parse(body);
-		if (answer.fan == true) {
-			child_process.exec("sudo python3 LIGHT_ON.py")
-		}
-		else{
-			child_process.exec("sudo python3 LIGHT_OFF.py")
-		}
+	child_process.exec("/opt/vc/bin/vcgencmd measure_temp", (err, stdout, stderr) => {
+		temperature = stdout;
+		let url = "http://address:port/report?t=" + temperature + "&id=" + id;
+		request(url, (error, response, body) => {
+			let answer = JSON.parse(body);
+			if (answer.fan == true) {
+				child_process.exec("sudo python3 LIGHT_ON.py")
+			}
+			else {
+				child_process.exec("sudo python3 LIGHT_OFF.py")
+			}
+		});
 	});
 }, 5000);
 
